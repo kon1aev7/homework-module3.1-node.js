ba05bdc7d6b7f4082f170a520c02662dcb1fd303
kon1aev7 <konyaevd1995@gmail.com>
2025-04-02 15:54:48 +0300

    sasa

---
+++ src/db/models/Movie.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+
+const movieSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['film', 'serial'],
+    default: 'film',
+    required: true,
+  },
+});
+
+const MovieCollection = model('movie', movieSchema);
+
+export default MovieCollection;
---
+++ src/services/movies.js
@@ -0,0 +1,3 @@
+import MovieCollection from '../db/models/Movie.js';
+export const getMovies = () => MovieCollection.find();
+export const getMovieById = (id) => MovieCollection.findOne({ _id: id });
